aa7f74abcf6389ea42bed7e79a02e072fdd46f52
Jodieadver <lisijia1028111@gmail.com>
2025-10-30 18:52:52 +0100

    First MVP

--- taskflow/resources/js/pages/ProjectForm.vue
+++ taskflow/resources/js/pages/ProjectForm.vue
@@ -25,6 +25,7 @@ const errors = (usePage().props as any).errors || {}
         <header>
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">Create a Project</h1>
+            </div>
 
 
         </header>
